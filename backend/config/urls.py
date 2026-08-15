1a7af5e09b91c2d27ba34d6768306bc7e50f5251
Ziyobek Sanaqulov <sanaqulovziyobek14@gmail.com>
2026-08-15 20:56:06 +0500

    first  commint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path("api/v1/", include("apps.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
-    path("api/auth/", include("authentication.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
